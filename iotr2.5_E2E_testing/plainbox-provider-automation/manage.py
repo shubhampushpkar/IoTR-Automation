a05599dd05c10d79f10c24420d4df04f38d2f80d
Shubham Pushpkar <shubham2.pushpkar@aricent.com>
2018-11-21 18:47:35 +0530

    first commit

---
+++ iotr2.5_E2E_testing/plainbox-provider-automation/manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from plainbox.provider_manager import setup, N_
+
+setup(
+    name='com.aricent.ggn:iotr2-automation',
+    version="1.0",
+    namespace='com.aricent.testing',
+    description=N_("The com.aricent.ggn:iotr2-automation provider"),
+    gettext_domain="com_aricent_ggn_iotr2-automation",
+)
